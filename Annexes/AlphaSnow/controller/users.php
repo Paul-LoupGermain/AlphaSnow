9ae69876f4dcb4454662269f8d0b2802e5f5c7bc
GERMAIN Paul-Loup <Paul-Loup.GERMAIN@cpnv.ch>
2022-02-11 14:32:54 +0100

    WIP

--- Annexes/AlphaSnow/controller/users.php
+++ Annexes/AlphaSnow/controller/users.php
@@ -11,6 +11,25 @@
         require "view/login.php";
     }
 
-    function register(){
-        require "view/register.php";
+    function register($login_request){
+        if ((isset($login_request['inputFirstName'])) && (isset($login_request['inputLastName'])) && (isset($login_request['inputEmailAddress'])) && (isset($login_request['inputPassword']))){
+            $inputFirstName = $login_request['inputFirstName'];
+            $inputLastName = $login_request['inputLastName'];
+            $inputEmailAddress = $login_request['inputEmailAddress'];
+            $inputPassword = $login_request['inputPassword'];
+
+            require_once "model/users_managment.php";
+            if (isLoginCorrect($inputFirstName, $inputLastName, $inputEmailAddress, $inputPassword)){
+                $_SESSION['inputEmailAddress']=$inputEmailAddress;
+                require "view/home.php";
+            }else{
+                $loginErrorMessage = "Erreur";
+                require "view/login.php";
+            }
+
+        }else{
+            $loginErrorMessage = "Erreur";
+            require "view/register.php";
+        }
     }
+
--- Annexes/AlphaSnow/model/users_managment.php
+++ Annexes/AlphaSnow/model/users_managment.php
@@ -7,20 +7,15 @@
      * @version   10-FEB-2022
      */
 
-    require "model/file_connector.php";
-
-    function save_register($array_input_user_form) {
-        $new_register_to_write = extract_register($array_input_user_form);
-        write_register_in_json($new_register_to_write);
-    }
-
-    function extract_register($array_input_user_form) {
-        $firstname = $array_input_user_form['inputFirstName'];
-        $lastname = $array_input_user_form['inputLastName'];
-        $emailAddress = $array_input_user_form['inputEmailAddress'];
-        $newsLetter = $array_input_user_form['inputNewsLetter'];
-        $new_register_temp = array($firstname, $lastname, $emailAddress, $newsLetter);
+    function isLoginCorrect($inputFirstName, $inputLastName, $inputEmailAddress, $inputPassword){
+        $firstname = $inputFirstName['inputFirstName'];
+        $lastname = $inputLastName['inputLastName'];
+        $emailAddress = $inputEmailAddress['inputEmailAddress'];
+        $newsPassword = $inputPassword['inputPassword'];
+        $new_register_temp = array($firstname, $lastname, $emailAddress, $newsPassword);
         return $new_register_temp;
+
+        require "model/file_connector.php";
     }
 
 ?>
