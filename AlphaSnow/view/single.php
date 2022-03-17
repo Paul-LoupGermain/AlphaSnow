a883da464f929d48184f58dbe2410f6ac3a368d4
GERMAIN Paul-Loup <Paul-Loup.GERMAIN@cpnv.ch>
2022-03-17 10:44:05 +0100

    WIP

--- AlphaSnow/view/single.php
+++ AlphaSnow/view/single.php
@@ -72,7 +72,7 @@
             </div>
             <div class="desc">
                 <h4>Description</h4>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores</p>
+                <p><?=$detail['description']; ?></p>
             </div>
         </div>
     </div>
